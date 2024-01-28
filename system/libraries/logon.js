ce100126d8ab80321f52cdc4080cf684a3da602d
danielhmetro <danielhmetro@gmail.com>
2024-01-28 14:40:17 -0500

    fixed 404 handling

--- system/libraries/logon.js
+++ system/libraries/logon.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
-	if (typeof errorUri !== 'undefined') {
-		makeWindow(false, errorUri, "/system/programs/error/?caller=404&path=" + errorUri, false, false, 500, 160)
+//	if (typeof errorUri !== 'undefined') {
+	var path = window.location.pathname.replace(/\//,'');
+	if (window.performance.getEntries()[0].responseStatus != 200) {
+		makeWindow(false, path, "/system/programs/error/?caller=404&path=" + path, false, false, 500, 160)
 	} else {
                 makeWindow(false, 'Welcome', '/system/programs/welcome/', false, false, 510, 350);
 		//makeWindow(false, 'Welcome to Windows', '/system/programs/logon/', false, false, 480, 185);
